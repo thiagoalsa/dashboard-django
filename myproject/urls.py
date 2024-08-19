f11378db8f17b528e615de79ce7daf44fa52e870
Thiago <contato@lysmatta.com>
2024-08-19 22:18:20 +0100

    change the name of header and title of site admin

--- myproject/urls.py
+++ myproject/urls.py
@@ -23,3 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+admin.site.site_header = 'Dashboard Administration'
+admin.site.site_title = 'Dashboard site admin'
