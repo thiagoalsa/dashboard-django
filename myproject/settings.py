9a737ebd5648fd360ef668c7c118e81657f8eebb
Thiago <contato@lysmatta.com>
2024-08-19 16:00:28 +0100

    create the path of static files and media files

--- myproject/settings.py
+++ myproject/settings.py
@@ -128,11 +128,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
