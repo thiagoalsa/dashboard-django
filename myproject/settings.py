a5ef039ec8d5be76fabbf374fb790ee373087f92
Thiago de Almeida Santos <thiago.as1994@gmail.com>
2024-10-06 17:04:34 +0100

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your_default_secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
